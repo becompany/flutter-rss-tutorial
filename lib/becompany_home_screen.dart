4089b08930e538e5abf210a7fc1af816148b272a
Javier Puerto <javier.puerto@becompany.ch>
2019-05-31 16:08:15 +0200

    Add list builder.

--- lib/becompany_home_screen.dart
+++ lib/becompany_home_screen.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
 
 class BecompanyHomeScreen extends StatelessWidget {
+  var news = ['News 1', 'News 2', 'News 3'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('BeCompany RSS'),
       ),
-      body: ListView(),
+      body: ListView.builder(
+          itemCount: news.length,
+          itemBuilder: (BuildContext ctxt, int index) {
+            return Card(
+                child: Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text(news[index]),
+            ));
+          }),
     );
   }
 }
