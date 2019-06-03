6d73d063865b70485deca490660e914a9e6a6245
Javier Puerto <javier.puerto@becompany.ch>
2019-06-03 16:27:09 +0200

    Use FutureBuilder.

--- lib/becompany_home_screen.dart
+++ lib/becompany_home_screen.dart
@@ -1,38 +1,59 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-import 'main.dart';
+import 'becompany_rss_service.dart';
 import 'webview_container.dart';
 
 class BecompanyHomeScreen extends StatelessWidget {
-
-  BecompanyHomeScreen();
-
   @override
   Widget build(BuildContext context) {
-    final feed = FeedStore.of(context).feed;
     return Scaffold(
       appBar: AppBar(
         title: Text('BeCompany RSS'),
       ),
-      body: ListView.builder(
-          itemCount: feed.items.length,
-          itemBuilder: (BuildContext ctxt, int index) {
-            final item = feed.items[index];
-            return ListTile(
-              title: Text(item.title),
-              subtitle: Text('Published at ' +
-                  DateFormat.yMd().format(DateTime.parse(item.published))),
-              contentPadding: EdgeInsets.all(16.0),
-              onTap: () async {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => WebViewContainer(
-                            item.id.replaceFirst('http', 'https'))));
-              },
-            );
-          }),
+      body: FutureBuilder(
+        future: BecompanyRssService().getFeed(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              if (snapshot.hasData) {
+                if (snapshot.data != null) {
+                  final feed = snapshot.data;
+                  return ListView.builder(
+                      itemCount: feed.items.length,
+                      itemBuilder: (BuildContext ctxt, int index) {
+                        final item = feed.items[index];
+                        return ListTile(
+                          title: Text(item.title),
+                          subtitle: Text('Published at ' +
+                              DateFormat.yMd()
+                                  .format(DateTime.parse(item.published))),
+                          contentPadding: EdgeInsets.all(16.0),
+                          onTap: () async {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => WebViewContainer(item
+                                        .id
+                                        .replaceFirst('http', 'https'))));
+                          },
+                        );
+                      });
+                }
+              }
+              break;
+            case ConnectionState.none:
+            case ConnectionState.active:
+            case ConnectionState.waiting:
+            default:
+              return Align(
+                alignment: Alignment.center,
+                child: CircularProgressIndicator(),
+              );
+          }
+        },
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:webfeed/domain/atom_feed.dart';
 
 import 'becompany_home_screen.dart';
-import 'becompany_rss_service.dart';
 
 void main() async {
-  final AtomFeed feed = await BecompanyRssService().getFeed();
-  runApp(MyApp(feed));
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final AtomFeed feed;
-
-  MyApp(this.feed);
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'BeCompany News',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -31,27 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: FeedStore(feed: feed, child: BecompanyHomeScreen()),
+      home: BecompanyHomeScreen(),
     );
   }
-}
-
-class FeedStore extends InheritedWidget {
-  final AtomFeed feed;
-
-  const FeedStore({
-    Key key,
-    @required this.feed,
-    @required Widget child,
-  })  : assert(feed != null),
-        assert(child != null),
-        super(key: key, child: child);
-
-  static FeedStore of(BuildContext context) {
-    return context.inheritFromWidgetOfExactType(FeedStore) as FeedStore;
-  }
-
-  @override
-  bool updateShouldNotify(FeedStore oldWidget) =>
-      feed.updated != oldWidget.feed.updated;
 }
